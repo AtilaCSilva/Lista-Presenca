f96137670afc1a76b501a3f615b08d4a447e78b5
AtilaCSilva <atilacsilva2008@gmail.com.br>
2022-12-10 13:07:26 -0300

    adding the Lista de Presença

---
+++ README.md
@@ -0,0 +1,22 @@
+# Lista de Presença
+> Praticando React
+
+
+[🔗Clique aqui para acessar](https://atilacsilva.github.io/Lista-Presenca) 
+
+## Sobre o Projeto
+Nesse projeto eu uso oque aprendi no curso de introdução de React na plataforma da rocketseat, para construir uma interface uma interface usando esses conhecimentos.
+
+
+## 🛠️ Tecnologias!
+- HTML5
+- CSS
+- JavaScript
+- React
+- NodeJS
+- Vite
+
+
+### ✍️ O que Aprendi!!
+Aprendi muito sobre React, praticamento toda base de como funciona e para que serve. 
+Vi sobre componentes, Como o react funciona com base no paradigma Funcional, sobre Estado, o princípio da imutabilidade e outros conceitos, que foram usados na prática desse simples projeto.
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react' // O useState é um exemplo de Hooks ( utilizar funções que conectão os recursos da Aplicação)
+import './styles.css'
+import { Card } from '../../components/Card'
+
+export function Home() {
+  const [studentName, setStudentName] = useState('');
+  const [ students, setStudents] = useState([])
+  const [user, setUser] = useState({avatar: ''})
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }),
+    }
+    setStudents(prevState => [...prevState, newStudent]) // '...' para pegar todo conteúdo do vetor e despejar ali dentro
+  }
+  
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/atilaCSilva')
+      const data = await response.json()
+      console.log("DADOS ===> ", data)
+
+      setUser({
+        avatar: data.avatar_url,
+      })
+    }
+
+
+    fetchData()
+    
+  }, [])
+  
+
+  return (
+    <div className="container">
+      <header>
+         <h1>Lista de Presença</h1>
+          <div>
+            <strong>
+              <a href="https://github.com/atilaCSilva">Atila</a>
+            </strong>
+            <img src={user.avatar} alt="Foto de Perfil de Atila" />
+          </div>
+      </header>
+      
+      <input 
+      type="text" 
+      placeholder="Digite algo.."
+      onChange={e => setStudentName(e.target.value)}
+      />
+      <button
+      onClick={handleAddStudent}
+      >Adicionar</button>
+      {
+        students.map(student => (
+          <Card 
+          key={student.time} // Ao usar uma Estru.Reo, é necessário uma chave única de identificação.
+          name={student.name} 
+          time={student.time}/>
+        ))
+      }
+      
+    </div>
+  )
+}
+
